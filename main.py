56539fe8c2f46be5d260c3a6ff620bccc070efc7
Shrine <nischalthapa10@hotmail.com>
2023-06-08 04:35:44 +0545

    added main.py

--- main.py
+++ main.py
@@ -1,59 +1,122 @@
+import random as rd
+
 MAX_LINES = 3
 MAX_BET = 1000
 
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 3,
+    "C": 6,
+    "D": 8,
+}
+
+
+def get_slot_machine_spin(rows,cols,symbols):
+    all_symbols = []
+
+    # list comprehension  --- 1
+    count = [symbols for symbols, counts in symbol_count.items() for _ in range(counts)]
+    print(count)
+
+    # simple method       --- 2 s
+    for symbols, counts in symbol_count.items():
+        print(counts)
+        for _ in range(counts):
+            all_symbols.append(symbols)
+
+    print(all_symbols)
+    columns = []
+    
+    for _ in range(rows):
+        column = []
+        current_symbols = all_symbols[:]# : colon essential for copying not reference copy but creating a new reference 
+        
+        for _ in range(cols):
+            value = rd.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+            
+        columns.append(column)
+             
+    return columns
+
+def print_slot_machine(columns):
+    i=0
+    while True:
+        for ind,value in enumerate(column for column in columns):
+            if i == ind:
+                j=0
+                current_value=[C_values for C_values in value]
+                for ind,value in enumerate(current_value):
+                    if j * 2 <= ind + 1 :
+                        
+                        print(value , end= " | ") 
+                        
+                    else:
+                        print(value , end="")
+                    j+=1 
+                print('\n')
+                i+=1
+            else:
+                pass  
+        break
 
 def deposit():
     while True:
-        
         print("desposit amount : $")
         # amount = input("What you desposit amount : $")
         amount = input()
-        
-        
-        # if not amount.isdigit(): or 
-        if  amount.isdigit()==False:
+
+        # if not amount.isdigit(): or
+        if amount.isdigit() == False:
             print("invalid amount")
-        else :
+        else:
             _amount = int(amount)
-            if _amount  <=0:
+            if _amount <= 0:
                 print("invalid amount")
-            else:    
-                # print(f"deposited amount :${amount}") 
+            else:
+                # print(f"deposited amount :${amount}")
                 return amount
-    
+
 
 def get_maxno_of_lines():
     while True:
         # print("no of lines you want to bet in ?")
-        
-        lines=input("no of lines you want to bet in  (1 - " + str(MAX_LINES) + ")?  : ")
-        lines_temp=lines
+
+        lines = input(
+            "no of lines you want to bet in  (1 - " + str(MAX_LINES) + ")?  : "
+        )
+        lines_temp = lines
         # print(lines_temp)
-        
-       
-        if lines.isdigit() ==False:
-            print("enter a valid number") 
-                
+
+        if lines.isdigit() == False:
+            print("enter a valid number")
+
         else:
-            n=int(lines_temp)
+            n = int(lines_temp)
             # print(n)
-            if n <= 0 or n > MAX_LINES :
+            if n <= 0 or n > MAX_LINES:
                 print("inavlid no of lines please enter from the specified range")
             else:
                 # print(f"no of lines you want to bet in : {lines}")
                 return lines
+
+
 def get_bet():
     while True:
         bet = input("what would you like to bet : ")
         bet_temp = bet
-        
+
         if bet.isdigit() == False:
             print("invalid bet")
-        
+
         else:
-            n=int(bet_temp)
-            if not  n > 0  or not n <= MAX_BET:
-                print("invalid bet amount") 
+            n = int(bet_temp)
+            if not n > 0 or not n <= MAX_BET:
+                print("invalid bet amount")
             else:
                 # print(bet)
                 return bet
@@ -61,7 +124,9 @@ def get_bet():
     # else:
     #     print("no balance")
     #     return bet
-def get_bet_check(balance,lines):
+
+
+def get_bet_check(balance, lines):
     while True:
         # bet = get_bet()
         # total = bet*lines
@@ -69,20 +134,21 @@ def get_bet_check(balance,lines):
         bet = int(get_bet())
         total = lines * bet
         print(total)
-        
-        if balance < total:
-            print("not enough balance")#no line after this so it goes back to the start of the loop which is the While True : statement
 
-            
+        if balance < total:
+            print(
+                "not enough balance"
+            )  # no line after this so it goes back to the start of the loop which is the While True : statement
+
         else:
             # final_balance = balance - total
             break
             # print(f"your total after youve placed youre bet will be ${final_balance}")
-    print(f"you are betting ${bet} on lines : {lines} for a total of ${total}")
-          
+        print(f"you are betting ${bet} on lines : {lines} for a total of ${total}")
+    return True
 
-        
-# get_maxno_of_lines() 
+
+# get_maxno_of_lines()
 def main():
     balance = deposit()
     lines = int(get_maxno_of_lines())
@@ -90,12 +156,14 @@ def main():
     # total = lines * bet
     # print(total)
     # int(get_bet_check(balance,total))
-    get_bet_check(int(balance),lines)
-    
+    value = get_bet_check(int(balance), lines)
+    if value == True:
+        columns=get_slot_machine_spin(ROWS,COLS,symbol_count)
+        print_slot_machine(columns)
+
 
 main()
 
 
 # if __name__=="__main__":
 #     print("amount stored succesfully")
-        
